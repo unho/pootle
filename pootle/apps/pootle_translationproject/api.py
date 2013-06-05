3561a35b4b203f80b7ef321a9409ea9b693b3cd8
Leandro Regueiro <leandro.regueiro@gmail.com>
2013-06-05 21:13:59 +0200

    UNDO    first attempt to fix bug 2281

--- pootle/apps/pootle_translationproject/api.py
+++ pootle/apps/pootle_translationproject/api.py
@@ -17,13 +17,22 @@
 # You should have received a copy of the GNU General Public License along with
 # this program; if not, see <http://www.gnu.org/licenses/>.
 
+try:
+    from django.conf.urls import url
+except ImportError: # Django < 1.4
+    from django.conf.urls.defaults import url
+from django.db.models import Q
+
 from tastypie import fields
 from tastypie.authentication import BasicAuthentication
 from tastypie.authorization import DjangoAuthorization
+from tastypie.utils import trailing_slash
 
 from pootle.core.api import StatisticsModelResource
 from pootle_misc.stats import get_raw_stats
 from pootle_store.api import StoreResource
+from pootle_store.models import Unit
+from pootle_store.util import OBSOLETE, UNTRANSLATED, FUZZY
 from pootle_translationproject.models import TranslationProject
 
 
@@ -47,9 +56,67 @@ class TranslationProjectResource(StatisticsModelResource):
         list_allowed_methods = ['post']
         # HTTP methods allowed for visiting /statistics/ URLs
         statistics_allowed_methods = ['get']
+        # List of fields shown when visiting /need-work-units/
+        need_work_units_fields = [
+            'language',
+            'project',
+            'units',
+        ]
+        # HTTP methods allowed for visiting /need-work-units/ URLs
+        need_work_units_allowed_methods = ['get']
         authorization = DjangoAuthorization()
         authentication = BasicAuthentication()
+
+    def prepend_urls(self):
+        """Returns a list of urlpatterns to be prepend to the default one."""
+        urls = super(TranslationProjectResource, self).prepend_urls()
+        urls += [
+            url(r"^(?P<resource_name>%s)/(?P<%s>\w[\w/-]*)/need-work-units%s$" %
+                (self._meta.resource_name, self._meta.detail_uri_name,
+                trailing_slash()), self.wrap_view('dispatch_need_work_units'),
+                name="api_dispatch_need_work_units"),
+        ]
+        return urls
+
+    def dispatch_need_work_units(self, request, **kwargs):
+        """Handles the HTTP methods on a single TP 'need-work' units list.
+
+        Relies on ``Resource.dispatch`` for the heavy-lifting.
+        """
+        return self.dispatch('need_work_units', request, **kwargs)
+
+    def get_need_work_units(self, request, **kwargs):
+        """Just calls ``Resource.get_detail``
+
+        This gets called in ``Resource.dispatch``
+        """
+        return self.get_detail(request, **kwargs)
 
     def retrieve_statistics(self, bundle):
         """Retrieve the statistics for the current resource object."""
         return get_raw_stats(bundle.obj, include_suggestions=True)
+
+    def dehydrate(self, bundle):
+        """A hook to allow final manipulation of data.
+
+        It is run after all fields/methods have built out the dehydrated data.
+
+        Useful if you need to access more than one dehydrated field or want to
+        annotate on additional data.
+
+        Must return the modified bundle.
+        """
+        bundle = super(TranslationProjectResource, self).dehydrate(bundle)
+        if bundle.request.path.endswith("/need-work-units/"):
+            # Remove unnecessary fields for responses to /need_work_units/ URLs
+            for field in self._meta.fields:
+                if field not in self._meta.need_work_units_fields:
+                    bundle.data.pop(field, None)
+            criteria = {
+                'store__translation_project': bundle.obj,
+            }
+            unfinished = Unit.objects.filter(**criteria)
+            unfinished = unfinished.filter(Q(state=FUZZY) | Q(state=OBSOLETE) |
+                                           Q(state=UNTRANSLATED))
+            bundle.data['units'] = unfinished#TODO look for a way of putting here a list of resource URIs to UnitResources
+        return bundle
